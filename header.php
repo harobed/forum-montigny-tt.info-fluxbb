56a7b0300eac6de4f0406742eec48ce13b3c4c88
Stéphane Klein <sklein@bearstech.com>
2013-02-16 10:48:57 +0100

    Ajout du ribbon Forkme on github

--- header.php
+++ header.php
@@ -163,6 +163,7 @@ if (isset($focus_element))
 	$tpl_main = str_replace('<body>', '<body onload="document.getElementById(\''.$focus_element[0].'\').elements[\''.$focus_element[1].'\'].focus()">', $tpl_main);
 }
 // END SUBST - <body>
+$tpl_main = str_replace('</body>', '<a href="https://github.com/harobed/forum.montigny-tt.info"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a></body>', $tpl_main);
 
 
 // START SUBST - <pun_page>
