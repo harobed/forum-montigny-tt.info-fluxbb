2bdf7bc57770ef2a915c4b8081d9328cc681d6f1
Stéphane Klein <sklein@bearstech.com>
2013-02-17 23:53:59 +0100

    Append piwik script

--- header.php
+++ header.php
@@ -163,7 +163,7 @@ if (isset($focus_element))
 	$tpl_main = str_replace('<body>', '<body onload="document.getElementById(\''.$focus_element[0].'\').elements[\''.$focus_element[1].'\'].focus()">', $tpl_main);
 }
 // END SUBST - <body>
-$tpl_main = str_replace('</body>', '<a href="https://github.com/harobed/forum.montigny-tt.info"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a></body>', $tpl_main);
+$tpl_main = str_replace('</body>', '<a href="https://github.com/harobed/forum.montigny-tt.info"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a><!-- Piwik --><script type="text/javascript">var pkBaseURL = (("https:" == document.location.protocol) ? "https://piwik.stephane-klein.info/" : "http://piwik.stephane-klein.info/");document.write(unescape("%3Cscript src=\'" + pkBaseURL + "piwik.js\' type=\'text/javascript\'%3E%3C/script%3E"));</script><script type="text/javascript">try { var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 3); piwikTracker.trackPageView(); piwikTracker.enableLinkTracking(); } catch( err ) {}</script><noscript><p><img src="http://piwik.stephane-klein.info/piwik.php?idsite=3" style="border:0" alt="" /></p></noscript><!-- End Piwik Tracking Code --></body>', $tpl_main);
 
 
 // START SUBST - <pun_page>
